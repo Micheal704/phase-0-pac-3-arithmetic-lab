fe55ba80f1a304d1f25f5ee433b99f9a88bdb80b
Micheal704 <engineering704@yahoo.com>
2023-08-13 00:36:56 -0400

    arthmetic lab

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(){
+return (add)
+
+}
+
+function subtract(){
+    return(subtract)
+}
+
+function multiply(){
+    return(multiply)
+}
+
+
+
+
+function divide(){
+    return(divide)
+}
+
+function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+
+  function divide(a, b) {
+    return a / b;
+  }
+
+
+  function increment(number) {
+    return number + 1;
+}
+  
+
+function decrement(number) {
+    return number - 1;
+}
+
+function makeInt(string){
+    return parseInt(string,10)
+  }
+
+  function preserveDecimal(string){
+    return parseFloat(string)
+  }
